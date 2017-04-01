c7568d2f9400e1bb524e4265951e2105348b650f
tc-imba <liuyh615@sjtu.edu.cn>
2017-04-01 15:11:25 +0800

    File & FileDefinition

---
+++ src/com/tcimba/idea/latex/LatexFile.java
@@ -0,0 +1,37 @@
+package com.tcimba.idea.latex;
+
+/**
+ * Created by liu on 2017/4/2.
+ */
+
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import com.tcimba.idea.latex*;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class LatexFile extends PsiFileBase {
+
+    public LatexFile(@NotNull FileViewProvider viewProvider) {
+        super(viewProvider, LatexLanguage.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public FileType getFileType() {
+        return LatexFileType.INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+        return LatexLanguage.NAME + " File";
+    }
+
+    @Override
+    public Icon getIcon(int flags) {
+        return super.getIcon(flags);
+    }
+
+}
